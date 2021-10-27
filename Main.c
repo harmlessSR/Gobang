685360587134605dc4830bd617d972a387b61020
harmlessSR <harmlesssr@gmail.com>
2021-10-27 19:48:59 +0800

    写完了评估函数，还未调试

--- Main.c
+++ Main.c
@@ -16,19 +16,29 @@ int nWinner;                //储存最后赢家,1黑2白
 void InitBoard();           //初始化棋盘
 void ShowBoard();           //显示棋盘
 void SelectMode();          //选择游戏模式
-int PlayGobang();           //五子棋内容
+void PlayGobang();           //五子棋内容
 
 //将棋形读取进Pattern,x,y为目标点位,Index表征以何方视角计分（BLACK/WHITE）empty_flag用于表征是否假想如果在这里下棋会怎样，若是填1
 void GetPattern(int x,int y,int Index,int empty_flag); 
 //getpattern的辅助函数，会返回此时在pattern中应该为什么数字;BoardContent:棋盘上的内容，一般填Board[][],Index同上      
 int PatternNumber(int BoardContent,int Index);        
-//比较两个字符串是否能匹配，source是需比较的,target是目标,匹配成功返回1    
+//比较两个字符串是否能匹配，source是需比较的,target是目标,返回source中有几个target字段
 int ComparePattern(int source[],int target[]);  
-//给棋形打分（默认为我方立场），返回得分
-
+//给棋形打分，返回得分,index表示是算我方得分还是算阻止敌方得分,我为1敌方为2
+int AssessPattern(int index);
+//对棋盘上空置的一点评估得出得分，返回分数
+int AssessPoint(int x,int y);
+//遍历棋盘返回分数最高的点
+void GetPoint();
+int BestPoint[2];   //储存getpoint找到的最好点
 
 int main()
 {
+    /*保留用于测试comparepattern的部分，后续优化*/
+    // int testsource[15] = {1,2,1,2,1,6,7,8,9,10,11,12,13,14,15};
+    // int testtarget[] = {1,2,1,99};
+    // printf("%d",ComparePattern(testsource, testtarget));
+    // system("pause");
 
     SelectMode();       //选择模式 
     InitBoard();        //初始化棋盘
@@ -185,7 +195,6 @@ int PlayGobang()
             Board[tBlackx][tBlacky] = TemBLACK;
             ShowBoard();
             
-            GetPattern(tBlackx,tBlacky,BLACK,0);
             /*白子人下*/
             if(iFlag_FirstRound == 1){
                 Board[tWhitex][tWhitey] = WHITE;
@@ -201,7 +210,37 @@ int PlayGobang()
             if(iFlag_FirstRound == 0) iFlag_FirstRound = 1;     //调整不再是第一局
         }
     }   
-    return 0;  
+   
+   if(nMode == 2){
+        while(1){
+            /* 黑子机器下 */
+            if(iFlag_FirstRound == 1){
+                Board[tBlackx][tBlacky] = BLACK;        //如果不是第一局，将前一步调整为通常显示
+            }
+            nFlag = 1;
+            scanf("%c%d",&tBlackx,&tBlacky);
+            getchar();
+            tBlackx = tBlackx - 'a';
+            tBlacky = tBlacky - 1;
+            Board[tBlackx][tBlacky] = TemBLACK;
+            ShowBoard();
+            
+            /*白子人下*/
+            if(iFlag_FirstRound == 1){
+                Board[tWhitex][tWhitey] = WHITE;
+            }
+            nFlag = 2;
+            scanf("%c%d",&tWhitex,&tWhitey);
+            getchar();
+            tWhitex = tWhitex - 'a';
+            tWhitey = tWhitey - 1;
+            Board[tWhitex][tWhitey] = TemWHITE;
+            ShowBoard();
+
+            if(iFlag_FirstRound == 0) iFlag_FirstRound = 1;     //调整不再是第一局
+        }
+   }
+     
 }
 
 void GetPattern(int x,int y,int Index,int empty_flag)
@@ -257,7 +296,7 @@ void GetPattern(int x,int y,int Index,int empty_flag)
     //恢复至原始状态
     }
 
-//0为空 1为我方 2为敌方 3为墙
+//0为空 1为我方 2为敌方或墙
 int PatternNumber(int a,int Index)
 {
     if(a == WHITE || a == TemWHITE){
@@ -273,11 +312,79 @@ int PatternNumber(int a,int Index)
 
 int ComparePattern(int source[],int target[])
 {
+    int count = 0;
+    
     for(int i = 0; i < 14; i++){         //target的第一位与source的哪一位匹配
         for(int j = 0; source[i+j] == target[j] && ((i+j) <= 14) && target[j] != '\0'; j++)
-            if(target[j+1] == 99)  return 1;       //99表示棋形代码中止
+            if(target[j+1] == 99){
+                count++;
+                break;
+            }       //99表示棋形代码中止
     }
     
-    return 0;
+    return count;
 }
 
+int AssessPattern(int index)
+{
+    int p;
+    int score = 0;
+    if(index == 1) p = 0;
+    else if(index == 2) p = 1;
+    else printf("error in assesspattern\n");        //选择使用评分表的哪一行
+    
+    int TargetPatterns[8][7] = {{1,1,1,1,1,99},{0,1,1,1,1,0,99},{0,1,1,1,1,2,99},{2,1,1,1,1,0,99},{0,1,1,1,0,99},{0,1,1,1,2,99},{2,1,1,1,0,99},{0,1,1,0,99}};
+    int Scores[8][2] = {{10000,1000},{200,100},{50,20},{50,20},{30,10},{8,5},{8,5},{2,1}};
+    //棋形与分数对照表，目前还很简陋
+
+    for(int i = 0; i < 4; i++ ){
+        for(int j = 0; j < 8; j++){
+            score += Scores[j][p] * ComparePattern(Pattern[i],TargetPatterns[j]);
+        }
+    }
+    
+    return score;
+}
+
+int AssessPoint(int x,int y)
+{
+    int Before_we, After_we, Before_they, After_they; //我方敌方在此点下棋之前之后得分
+    int score;
+
+    int we, they;
+    if(nMode == 2){
+        we = BLACK;
+        they = WHITE;
+    }
+    else if(nMode == 3){
+        we = WHITE;
+        they = BLACK;
+    }                           //确定敌我颜色
+
+    GetPattern(x,y,we,0);
+    Before_we = AssessPattern(1);
+    GetPattern(x,y,we,1);
+    After_we = AssessPattern(1);
+    GetPattern(x,y,they,0);
+    Before_they = AssessPattern(2);
+    GetPattern(x,y,they,1);
+    After_they = AssessPattern(2);      //算分
+
+    score = After_we - Before_we + After_they - Before_they;
+    return score;
+}
+
+void GetPoint()
+{
+    int HiScore = 0,score;
+    for(int x = 0; x<15; x++){
+        for(int y = 0; y<15 && Board[x][y] != WHITE && Board[x][y] != TemWHITE && Board[x][y] != BLACK && Board[x][y] != TemBLACK){
+            score = AssessPoint(x,y);
+            if(score > HiScore){
+                HiScore = score;
+                BestPoint[0] = x;
+                BestPoint[1] = y;
+            }
+        }
+    }
+}
